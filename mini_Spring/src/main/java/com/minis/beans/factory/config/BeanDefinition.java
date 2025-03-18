24e6ed360b78590b951d86877ca9b15725566749
YuLong <1003716039@qq.com>
2025-03-18 12:59:13 +0800

    20250318 手写 Spring 框架--扩展 BeanDefinition.java, 增加 scope、lazyInit 等属性

--- mini_Spring/src/main/java/com/minis/beans/factory/config/BeanDefinition.java
+++ mini_Spring/src/main/java/com/minis/beans/factory/config/BeanDefinition.java
@@ -5,12 +5,38 @@ package com.minis.beans.factory.config;
  * @author YuLong
  */
 public class BeanDefinition {
+    String SCOPE_SINGLETON = "singleton";
+    String SCOPE_PROTOTYPE = "prototype";
+    /**
+     * 是否懒加载, 加载时是否初始化
+     */
+    private boolean lazyInit = false;
+    /**
+     * bean 之间的依赖关系
+     */
+    private String[] dependsOn;
+    private ArgumentValues constructorArguments;
+    private PropertyValues propertyValues;
+    /**
+     * 初始化方法
+     */
+    private String initMethodName;
+    private volatile Object beanClass;
     private String id;
     private String className;
+    private String scope = SCOPE_SINGLETON;
 
     public BeanDefinition(String id, String className) {
         this.id = id;
         this.className = className;
+    }
+
+    public boolean isSingleton() {
+        return SCOPE_SINGLETON.equals(scope);
+    }
+
+    public boolean isPrototype() {
+        return SCOPE_PROTOTYPE.equals(scope);
     }
 
     public String getId() {
@@ -27,5 +53,77 @@ public class BeanDefinition {
 
     public void setClassName(String className) {
         this.className = className;
+    }
+
+    public String getSCOPE_SINGLETON() {
+        return SCOPE_SINGLETON;
+    }
+
+    public void setSCOPE_SINGLETON(String SCOPE_SINGLETON) {
+        this.SCOPE_SINGLETON = SCOPE_SINGLETON;
+    }
+
+    public String getSCOPE_PROTOTYPE() {
+        return SCOPE_PROTOTYPE;
+    }
+
+    public void setSCOPE_PROTOTYPE(String SCOPE_PROTOTYPE) {
+        this.SCOPE_PROTOTYPE = SCOPE_PROTOTYPE;
+    }
+
+    public boolean isLazyInit() {
+        return lazyInit;
+    }
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+
+    public String[] getDependsOn() {
+        return dependsOn;
+    }
+
+    public void setDependsOn(String[] dependsOn) {
+        this.dependsOn = dependsOn;
+    }
+
+    public ArgumentValues getConstructorArguments() {
+        return constructorArguments;
+    }
+
+    public void setConstructorArguments(ArgumentValues constructorArguments) {
+        this.constructorArguments = constructorArguments;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public Object getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Object beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
     }
 }
