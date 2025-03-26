bd294fe6a79a59209bb7b11b6bb4694958b71544
YuLong <1003716039@qq.com>
2025-03-27 00:56:05 +0800

    20250327 手写 Spring 框架--自动装配工厂类

--- mini_Spring/src/main/java/com/minis/beans/factory/config/AutowireCapableBeanFactory.java
+++ mini_Spring/src/main/java/com/minis/beans/factory/config/AutowireCapableBeanFactory.java
@@ -1,39 +1,58 @@
 package com.minis.beans.factory.config;
 
 import com.minis.beans.BeansException;
-import com.minis.beans.factory.BeanFactory;
+import com.minis.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import com.minis.beans.factory.support.AbstractBeanFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author YuLong
  */
-public class AutowireCapableBeanFactory implements BeanFactory {
-    @Override
-    public Object getBean(String beanName) throws BeansException {
-        return null;
+public class AutowireCapableBeanFactory extends AbstractBeanFactory {
+
+    private final List<AutowiredAnnotationBeanPostProcessor> beanPostProcessors = new ArrayList<>();
+
+    public void addBeanPostProcessor(AutowiredAnnotationBeanPostProcessor beanPostProcessor) {
+        this.beanPostProcessors.remove(beanPostProcessor);
+        this.beanPostProcessors.add(beanPostProcessor);
+    }
+
+    /**
+     * 获取BeanPostProcessor的数量
+     * @return
+     */
+    public int getBeanPostProcessorCount() {
+        return this.beanPostProcessors.size();
+    }
+
+    public List<AutowiredAnnotationBeanPostProcessor> getBeanPostProcessors() {
+        return this.beanPostProcessors;
     }
 
     @Override
-    public boolean containsBean(String beanName) {
-        return false;
+    public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (AutowiredAnnotationBeanPostProcessor beanPostProcessor : this.beanPostProcessors) {
+            beanPostProcessor.setBeanFactory(this);
+            result = beanPostProcessor.postProcessBeforeInitialization(result, beanName);
+            if (result == null) {
+                return result;
+            }
+        }
+        return result;
     }
 
     @Override
-    public void registerBean(String beanName, Object obj) {
-
-    }
-
-    @Override
-    public boolean isSingleton(String beanName) {
-        return false;
-    }
-
-    @Override
-    public boolean isPrototype(String beanName) {
-        return false;
-    }
-
-    @Override
-    public Class<?> getType(String beanName) {
-        return null;
+    public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (AutowiredAnnotationBeanPostProcessor beanPostProcessor : this.beanPostProcessors) {
+            result = beanPostProcessor.postProcessAfterInitialization(result, beanName);
+            if (result == null) {
+                return result;
+            }
+        }
+        return result;
     }
 }
